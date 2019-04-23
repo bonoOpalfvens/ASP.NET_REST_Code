39f982d56f940697a991e84389d68704907ff90c
Bono Opalfvens <bono-opalfvens@hotmail.com>
2019-04-23 12:00:33 +0200

    [Feat] Add Post-, BoardController + Remove default Controller

---
+++ REST_Code/Controllers/BoardController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace REST_Code.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardController : Controller
+    {
+    }
+}
