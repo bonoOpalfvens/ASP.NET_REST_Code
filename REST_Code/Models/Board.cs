19d210953a58e7a7608ecc3ffc09aa83bfa4ae7e
Bono Opalfvens <bono-opalfvens@hotmail.com>
2019-04-23 12:12:07 +0200

    [Feat] Add Board and Post Class

---
+++ REST_Code/Models/Board.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_Code.Models
+{
+    public class Board
+    {
+        #region Properties
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public String Icon { get; set; }
+        public ICollection<Post> Posts { get; set; }
+        #endregion
+
+        #region Constructors
+        public Board()
+        {
+            Posts = new List<Post>();
+        }
+        #endregion
+
+        #region Methods
+        public Post GetPost(long id) => Posts.SingleOrDefault(i => i.Id == id);
+        #endregion
+    }
+}
---
+++ REST_Code/Models/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace REST_Code.Models
+{
+    public class Post
+    {
+        #region Properties
+        public long Id { get; set; }
+        [Required]
+        public String Title { get; set; }
+        public ICollection<Board> Boards { get; set; }
+        public String User { get; set; }
+        public DateTime DateAdded { get; set; }
+        public ICollection<String> Comments { get; set; }
+        public ICollection<String> Likes { get; set; }
+        #endregion
+
+        #region Constructors
+        public Post()
+        {
+            Boards = new List<Board>();
+            DateAdded = DateTime.Now;
+            Comments = new List<String>();
+            Likes = new List<String>();
+        }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
